9da2c843c7aca1d01130c4f4385ec415cc6284a0
Vibhab Timsina <vibhabtimsina86@gmail.com>
2026-06-29 21:28:33 +0545

    Better walking movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,61 +1,119 @@
 extends CharacterBody3D
 
+enum PlayerState {WALKING, GLIDING}
+var curr_player_state : PlayerState = PlayerState.WALKING
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export_group("Camera")
+@export_range(0.0, 1.0) var sensitivity : float = 0.25
+@onready var _camera_pivot: Node3D = $CameraPivot
+@onready var _camera: Camera3D = $CameraPivot/SpringArm3D/Camera3D
+
+@export_group("Movement")
+@export var move_speed: float = 15.0
+@export var acceleration: float = 20.0
+@export var rotation_speed: float = 12.0
+@export var jump_force: float = 18.0
+@export var gravity: float = -30.0 
 
 @onready var animation_player: AnimationPlayer = $characterMedium/AnimationPlayer
 @onready var character: Node3D = $characterMedium
 
-var is_gliding: bool = false
+var _camera_input_dir : Vector2 = Vector2.ZERO
+var _last_move_dir: Vector3 = Vector3.ZERO
 
-func player_movement(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+const GRAV: float = 30
+const GLIDE_GRAV : float = 5.0
+var GLIDE_SPEED : float = 0.0
 
-	move_and_slide()
+var LAST_WALK_VELOCITY: Vector3
 
-func player_animation() -> void:
-	if (velocity.x != 0 or velocity.z != 0):
-		animation_player.play("run/Root|Run")
-		return
+
+
+func walking_movement(delta: float) -> void:
+	character.rotation.x = 0
 	
+	_camera_pivot.rotation.x += _camera_input_dir.y * delta
+	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, -PI/6, PI/3)
+	_camera_pivot.rotation.y -= _camera_input_dir.x * delta
+	
+	var raw_input: Vector2 = Input.get_vector("left", "right", "forward", "backward")
+	var forward := _camera.global_basis.z
+	var right := _camera.global_basis.x
+	
+	var move_direction :Vector3 = forward * raw_input.y + right * raw_input.x
+	move_direction.y = 0.0
+	move_direction = move_direction.normalized()
+	
+	var y_vel := velocity.y
+	velocity.y = 0.0
+	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
+	velocity.y = y_vel + gravity * delta
+	
+	var is_starting_jump : bool = Input.is_action_just_pressed("jump") and is_on_floor()
+	if is_starting_jump:
+		velocity.y += jump_force
+	
+	move_and_slide()
+	
+	if move_direction.length() > 0.2:
+		_last_move_dir = move_direction
+	var target_angle := Vector3.BACK.signed_angle_to(_last_move_dir, Vector3.UP)
+	character.global_rotation.y = lerp_angle(character.rotation.y, target_angle, rotation_speed * delta)
+
+
+func walking_animation()-> void:
 	if (velocity.y != 0):
 		animation_player.play("jump/Root|Jump")
+		return
+		
+	if (velocity.length() > 0.0):
+		animation_player.play("run/Root|Run")
 		return
 	
 	animation_player.play("idle/Root|Idle")
 
-func player_glide() -> void:
-	if is_on_floor():
-		character.rotation.x = 0
-		is_gliding = false
-		return
-	
-	#set model
+func glide_animation() -> void:
 	character.rotation.x = 90
+	animation_player.play("idle/Root|0_Targeting Pose")
+
+func glide_movement(delta: float) -> void:
+	velocity += Vector3(LAST_WALK_VELOCITY.x, -GLIDE_GRAV, LAST_WALK_VELOCITY.z) * delta
 	
+	move_and_slide()
+
+func glide_check() -> void:
+	if is_on_floor():
+		GLIDE_SPEED = 0.0
+		curr_player_state = PlayerState.WALKING
+
 
 func _physics_process(delta: float) -> void:
-	player_movement(delta)
-	player_animation()
-	if is_gliding : player_glide()
+	match curr_player_state:
+		PlayerState.WALKING:
+			walking_movement(delta)
+			walking_animation()
+		PlayerState.GLIDING:
+			glide_check()
+			glide_movement(delta)
+			glide_animation()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("left_click"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed and event.keycode == KEY_E:
-		is_gliding = true
+		curr_player_state = PlayerState.GLIDING
+	
+	var is_camera_motion : bool = (
+		event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	)
+	
+	if is_camera_motion:
+		_camera_input_dir = event.screen_relative * sensitivity 
